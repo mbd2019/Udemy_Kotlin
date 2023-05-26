8960577b5d7380fc1729a48bc8a1fa6bf71ae0a2
Marcos Blankenburg Dantas <hunterbd@hotmail.com>
2023-05-26 16:29:25 -0300

    Tipo Variavel Unsigned

--- Estudo_Kotlin/src/main/kotlin/Main.kt
+++ Estudo_Kotlin/src/main/kotlin/Main.kt
@@ -2,10 +2,24 @@ package Estudo_Kotlin.src.main.kotlin
 
 fun main() {
     println("Olá, Mundo")
-    println("Double: Max: " + Double.MAX_VALUE + " - Double: " + Double.MIN_VALUE)
+
+    /*Tipos de variáveis padrões*/
+    println("Double: Max: " + Double.MAX_VALUE + " - Min: " + Double.MIN_VALUE)
     println("Float: Max: " + Float.MAX_VALUE + " - Min: " + Float.MIN_VALUE)
-    println("Long: Max: " + Long.MAX_VALUE + " - Long: " + Long.MIN_VALUE)
+    println("Long: Max: " + Long.MAX_VALUE + " - Min: " + Long.MIN_VALUE)
     println("Int: Max: " + Int.MAX_VALUE + " - Min: " + Int.MIN_VALUE)
     println("Short: Max: " + Short.MAX_VALUE + " - Min: " + Short.MIN_VALUE)
     println("Byte: Max: " + Byte.MAX_VALUE + " - Min: " + Byte.MIN_VALUE)
+
+    /*Tipo de variáveis Unsigned*/
+    println("ULong MAX - ${ULong.MAX_VALUE} - ${ULong.MIN_VALUE}")
+    println("UInt MAX - ${UInt.MAX_VALUE} - ${UInt.MIN_VALUE}")
+    println("UShort MAX - ${UShort.MAX_VALUE} - ${UShort.MIN_VALUE}")
+    println("UByte MAX - ${UByte.MAX_VALUE} - ${UByte.MIN_VALUE}")
+
+    /*Instanciando variáveis do tipo Unsigned*/
+    val abc: UInt = 0u
+    val short: UShort = 10u
+    val byte: UByte = 10u
+    val long: ULong = 10u
 }
--- out/production/Udemy_Kotlin/README.md
+++ out/production/Udemy_Kotlin/README.md
@@ -1,2 +1,15 @@
 # Udemy_Kotlin
 Estudo de Kotlin na Udemy
+Para escrever comentários, no Kotlin, usa-se:
+
+// ou /**/
+
+Para escrever um comentário, mas com características de documentação, usa-se:
+
+Digite /** e tecla enter, que ficará dessa forma:
+
+/**
+
+*
+
+*/
