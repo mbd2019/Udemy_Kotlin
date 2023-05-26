1ffe8700eab4970863bd994544a6fbe59bcb69a3
Marcos Blankenburg Dantas <hunterbd@hotmail.com>
2023-05-26 08:27:44 -0300

    Tipo de Variaveis

--- Estudo_Kotlin/src/main/kotlin/Main.kt
+++ Estudo_Kotlin/src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
     println("Olá, Mundo")
+    println("Double: Max: " + Double.MAX_VALUE + " - Double: " + Double.MIN_VALUE)
+    println("Float: Max: " + Float.MAX_VALUE + " - Min: " + Float.MIN_VALUE)
+    println("Long: Max: " + Long.MAX_VALUE + " - Long: " + Long.MIN_VALUE)
+    println("Int: Max: " + Int.MAX_VALUE + " - Min: " + Int.MIN_VALUE)
+    println("Short: Max: " + Short.MAX_VALUE + " - Min: " + Short.MIN_VALUE)
+    println("Byte: Max: " + Byte.MAX_VALUE + " - Min: " + Byte.MIN_VALUE)
 }
